a94cb2c4ace73b25a488cb467727bb861cf7372d
M Zubair <md.zubair143598@gmail.com>
2023-07-10 17:48:53 +0500

    init22

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 
 import Countries from "@/components/Countries";
-import { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import Travel from "@/components/Travel";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import Head from "next/head";
 
 export default function Home() {
@@ -17,9 +17,8 @@ export default function Home() {
     return () => clearTimeout(timer);
   }, []);
   const getTextAlign = () => {
-    return i18n.language === 'ur' ? 'text-right ' : 'text-left';
+    return i18n.language === "ur" ? "text-right " : "text-left";
   };
-
 
   return (
     <>
@@ -28,24 +27,33 @@ export default function Home() {
       </Head>
 
       <main className="index h-screen">
-      {isContentVisible && (
-            <>
-      <Countries/>
-      <div  className={`lg:px-72 py-[40px] px-[10px] index font-bold tracking-[2px] pt-3 ${getTextAlign()}`}>
-          
+        {isContentVisible && (
+          <>
+            <Countries />
+            <div
+              className={`lg:px-72 py-[40px] px-[10px] index font-bold tracking-[2px] pt-3 ${getTextAlign()}`}
+            >
               <h2 className="text-[30px]  text-red-700">{t("title")}</h2>
 
               <p>{t("greeting")}</p>
-              <img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg" width={300} alt="" />
-            
-          <div className=' text-center flex flex-col  '>
-            <p>asdasd</p>
-          <img src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg" width={300} className='mt-3 ' alt="" />
-          </div>
-        </div>
-        </>
-          )}
-       {/* <Travel/> */}
+              <img
+                src="https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"
+                width={300}
+                alt=""
+              />
+
+              <div className=" text-center flex flex-col  ">
+                <p>asdasd</p>
+                <img
+                  src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg"
+                  width={300}
+                  className="mt-3 "
+                  alt=""
+                />
+              </div>
+            </div>
+          </>
+        )}
       </main>
     </>
   );
